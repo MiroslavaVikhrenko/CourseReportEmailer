0535751b2376b20c6c6159ba7ff0a42bff5b737d
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-07 18:31:03 -0700

    Practice serialize an object from C# to JSON and deserialize it back from JSON to C# using JsonConvert.SerializeObject() and JsonConvert.DeserializeObject()

--- CourseReportEmailer/Program.cs
+++ CourseReportEmailer/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace CourseReportEmailer
+﻿using CourseReportEmailer.Models;
+using Newtonsoft.Json;
+
+namespace CourseReportEmailer
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            EnrollmentDetailReportModel model = new EnrollmentDetailReportModel()
+            {
+                EnrollmentId = 1,
+                FirstName = "Mark",
+                LastName = "Hue",
+                CourseCode = "CA",
+                Description = "description",
+            };
+
+            //turn C# object into JSON object (= serialize)
+            var json = JsonConvert.SerializeObject(model);
+
+            //turn JSON object back to C# object
+            EnrollmentDetailReportModel objectFromJson = (EnrollmentDetailReportModel) JsonConvert.DeserializeObject(json, typeof(EnrollmentDetailReportModel));
+
             Console.ReadKey();
         }
     }
